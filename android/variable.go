8bbd5b9eba7c3631df285a0f24e8e169b9edaab1
Ethan Chen <intervigil@gmail.com>
2019-02-10 14:44:10 -0800

    Fix formatting
    Change-Id: If8fce98316be03fc9dabe27864bc4a324ab6fdb7

--- android/variable.go
+++ android/variable.go
@@ -312,7 +312,6 @@ func variableMutator(mctx BottomUpMutatorContext) {
 	valStruct := reflect.ValueOf(mctx.Config().productVariables)
 
 	doVariableMutation(mctx, a, variableValues, zeroValues, valStruct)
-
 }
 
 func doVariableMutation(mctx BottomUpMutatorContext, a *ModuleBase, variableValues reflect.Value, zeroValues reflect.Value,
@@ -325,10 +324,10 @@ func doVariableMutation(mctx BottomUpMutatorContext, a *ModuleBase, variableValu
 
 		// Check that the variable was set for the product
 		val := valStruct.FieldByName(name)
-        if val.IsValid() && val.Kind() == reflect.Struct {
+		if val.IsValid() && val.Kind() == reflect.Struct {
 			doVariableMutation(mctx, a, variableValue, zeroValue, val)
-            continue
-        } else if !val.IsValid() || val.Kind() != reflect.Ptr || val.IsNil() {
+			continue
+		} else if !val.IsValid() || val.Kind() != reflect.Ptr || val.IsNil() {
 			continue
 		}
 
