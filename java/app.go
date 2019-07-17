3fb5d2c70e48479b70bb2056270025a54d03138e
Jaewoong Jung <jungjw@google.com>
2019-07-17 10:21:49 -0700

    Add filename property to android_app_import
    Test: app_test.go
    Bug: 137218697
    Change-Id: If2b20a355bb6e9e8dbcd57347c0e2c6fa041a932

--- java/app.go
+++ java/app.go
@@ -689,6 +689,8 @@ type AndroidAppImport struct {
 	certificate *Certificate
 
 	dexpreopter
+
+	installPath android.OutputPath
 }
 
 type AndroidAppImportProperties struct {
@@ -714,6 +716,9 @@ type AndroidAppImportProperties struct {
 	// binaries would be installed by default (in PRODUCT_PACKAGES) the other binary will be removed
 	// from PRODUCT_PACKAGES.
 	Overrides []string
+
+	// Optional name for the installed app. If unspecified, it is derived from the module name.
+	Filename *string
 }
 
 // Chooses a source APK path to use based on the module and product specs.
@@ -836,7 +841,8 @@ func (a *AndroidAppImport) GenerateAndroidBuildActions(ctx android.ModuleContext
 
 	// TODO: Optionally compress the output apk.
 
-	ctx.InstallFile(installDir, a.BaseModuleName()+".apk", a.outputFile)
+	a.installPath = ctx.InstallFile(installDir,
+		proptools.StringDefault(a.properties.Filename, a.BaseModuleName()+".apk"), a.outputFile)
 
 	// TODO: androidmk converter jni libs
 }
