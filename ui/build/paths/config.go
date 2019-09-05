e2f73c9b8d17357d1097307a8e4a1e1e62cbb4ca
Sam Mortimer <sam@mortimer.me.uk>
2019-09-04 22:52:59 -0700

    soong: Whitelist nproc
    Change-Id: Ie83688c900a241728b1153cc8ac2aeccc2cb31f6
    Signed-off-by: xyyx <xyyx@mail.ru>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -104,6 +104,7 @@ var Configuration = map[string]PathConfig{
 	"javap":   Allowed,
 	"locale":  Allowed,
 	"lsof":    Allowed,
+	"nproc":   Allowed,
 	"openssl": Allowed,
 	"patch":   Allowed,
 	"pkg-config": Allowed,
