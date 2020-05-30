747524aa457fc947183764701f905b59dc989d65
Anay Wadhera <awadhera@berkeley.edu>
2020-05-29 23:42:11 -0700

    soong: whitelist ccache
    Change-Id: I68e5321804c8b1b4a6fcd4690de197440bef6ea6
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -76,6 +76,7 @@ func GetConfig(name string) PathConfig {
 var Configuration = map[string]PathConfig{
 	"bash":    Allowed,
 	"brotli":  Allowed,
+	"ccache":  Allowed,
 	"dd":      Allowed,
 	"diff":    Allowed,
 	"dlv":     Allowed,
