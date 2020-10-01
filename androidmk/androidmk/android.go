1a790d4b82090816724167ad3fb4981528e68fa5
Alessandro Astone <ales.astone@gmail.com>
2020-10-01 12:59:27 +0200

    soong: Add equivalent for LOCAL_EXPORT_CFLAGS
    Change-Id: Ieb3e5739b50789bdbaf41a7d5adb04b08f7b9ea2

--- androidmk/androidmk/android.go
+++ androidmk/androidmk/android.go
@@ -64,6 +64,7 @@ var rewriteProperties = map[string](func(variableAssignmentContext) error){
 	"LOCAL_SANITIZE_DIAG":                  sanitize("diag."),
 	"LOCAL_STRIP_MODULE":                   strip(),
 	"LOCAL_CFLAGS":                         cflags,
+	"LOCAL_EXPORT_CFLAGS":                  exportCflags,
 	"LOCAL_UNINSTALLABLE_MODULE":           invert("installable"),
 	"LOCAL_PROGUARD_ENABLED":               proguardEnabled,
 	"LOCAL_MODULE_PATH":                    prebuiltModulePath,
@@ -756,6 +757,13 @@ func cflags(ctx variableAssignmentContext) error {
 	ctx.mkvalue = ctx.mkvalue.Clone()
 	ctx.mkvalue.ReplaceLiteral(`\"`, `"`)
 	return includeVariableNow(bpVariable{"cflags", bpparser.ListType}, ctx)
+}
+
+func exportCflags(ctx variableAssignmentContext) error {
+	// The Soong replacement for EXPORT_CFLAGS doesn't need the same extra escaped quotes that were present in Make
+	ctx.mkvalue = ctx.mkvalue.Clone()
+	ctx.mkvalue.ReplaceLiteral(`\"`, `"`)
+	return includeVariableNow(bpVariable{"export_cflags", bpparser.ListType}, ctx)
 }
 
 func proguardEnabled(ctx variableAssignmentContext) error {
