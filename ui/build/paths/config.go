c029b2309b5d76a885330398d29f71278378d991
Joey Huab <joey@evolution-x.org>
2020-10-28 04:53:50 +0900

    soong: Whitelist bison & flex
    Change-Id: I89de9c5a7b00753b67485f1e70030f63d551e9cb

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -75,6 +75,7 @@ func GetConfig(name string) PathConfig {
 
 var Configuration = map[string]PathConfig{
 	"bash":    Allowed,
+	"bison":   Log,
 	"brotli":  Allowed,
 	"ccache":  Allowed,
 	"date":    Allowed,
@@ -82,6 +83,7 @@ var Configuration = map[string]PathConfig{
 	"diff":    Allowed,
 	"dlv":     Allowed,
 	"expr":    Allowed,
+	"flex":    Log,
 	"flock":   Allowed,
 	"fuser":   Allowed,
 	"getopt":  Allowed,
