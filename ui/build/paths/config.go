dbebc4f15c20c98042ac9eff1749bfafaebea03c
SagarMakhar <sagarmakhar@gmail.com>
2021-09-18 05:55:46 +0000

    Allow kernel build tools
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Change-Id: I148f36d59ddfa256d95219b5c250e79cfb72cd8d
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -127,7 +127,8 @@ var Configuration = map[string]PathConfig{
 	"tr":      Allowed,
 	"unzip":   Allowed,
 	"zip":     Allowed,
-
+	"arm-linux-androidkernel-as": Allowed,
+	"arm-linux-androidkernel-ld": Allowed,
 	// Host toolchain is removed. In-tree toolchain should be used instead.
 	// GCC also can't find cc1 with this implementation.
 	"ar":         Forbidden,
