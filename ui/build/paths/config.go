a277b3c3aa28eeb07f9ba6d9e3cb3390d644f33a
Alvin Francis <nivlafx@gmail.com>
2023-11-14 18:15:24 -0400

    soong: Whitelist yes
    Disallowed PATH tool "yes" used: []string{"yes", ""}
    See https://android.googlesource.com/platform/build/+/master/Changes.md
    
    Change-Id: I1286b1ddb8e315b067bf50840c43a4f0ca1c7f10
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -129,6 +129,8 @@ var Configuration = map[string]PathConfig{
 	"zip":     Allowed,
 	"arm-linux-androidkernel-as": Allowed,
 	"arm-linux-androidkernel-ld": Allowed,
+	"yes":     Allowed,
+
 	// Host toolchain is removed. In-tree toolchain should be used instead.
 	// GCC also can't find cc1 with this implementation.
 	"ar":         Forbidden,
